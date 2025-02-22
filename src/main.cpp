80340b92e5f330092cb45b0a3da1c00b2907b78a
Bryce Hackel <34104885+bhackel@users.noreply.github.com>
2025-02-22 13:27:48 -0800

    Enum and switch for launch sequence

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+enum State {
+    PRE_ARM,
+    ABORT,
+    ARMED,
+    LAUNCH
+};
+
+State currentState = PRE_ARM; // Initial state
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    // Setup code is run once at the beginning
+    Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // Main loop code is run repeatedly, like a while loop
+    switch (currentState) {
+        case PRE_ARM:
+            // Pre-arm state code
+            // Emit off for all valves
+            break;
+        case ABORT:
+            // Abort state code
+            // Disable all flow valves, enable all vent valves
+            break;
+        case ARMED:
+            // Armed state code
+            // Enable the pilot flow valve
+            break;
+        case LAUNCH:
+            // Launch state code
+            // Enable the fuel flow and oxidizer flow valve
+            break;
+    }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
